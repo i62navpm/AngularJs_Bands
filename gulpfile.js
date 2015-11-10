cf73524a3a9c15ab8a83da933c674f018956cc09
Manuel Navarro <manuelnavarro1987@gmail.com>
2015-11-10 23:27:50 +0100

    Inserción de tareas gulp

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+'use strict';
+
+var gulp    = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var wiredep = require('wiredep').stream;
+
+var paths = {
+              jsFiles        : ['./src/app/**/*.js'],
+              scssFiles      : ['./src/content/styles/*.scss'],
+              imagesFiles    : ['./src/content/assets/*.png'],
+              htmlFiles      : ['./src/app/**/*.html', '!./src/app/commons/directives/**/*.html'],
+              htmlDirectives : ['./src/app/commons/directives/**/*.html']
+            };
+
+gulp.task('prepareJS', function () {
+  gulp.src(paths.jsFiles)
+    .pipe(plugins.plumber())
+    .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.jshint())
+      .pipe(plugins.jshint.reporter('default'))
+      .pipe(plugins.concat('all.min.js'))
+      .pipe(plugins.ngAnnotate({
+          add: true
+        }))
+      .pipe(plugins.uglify())
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest('./build/js/'));
+});
+
+gulp.task('prepareCSS', function () {
+  gulp.src(paths.scssFiles)
+    .pipe(plugins.plumber())
+    .pipe(plugins.sass().on('error', plugins.sass.logError))
+    .pipe(plugins.concat('all.min.css'))
+    .pipe(plugins.autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: true
+    }))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('prepareImages', function(){
+  return gulp.src(paths.imagesFiles)
+  .pipe(plugins.imagemin())
+  .pipe(gulp.dest('./build/assets'));
+});
+
+gulp.task('prepareLibs', function () {
+  gulp.src('./src/app/index.html')
+    .pipe(wiredep())
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('prepareTemplates', function () {
+  return gulp.src(paths.htmlFiles)
+    .pipe(plugins.angularTemplatecache({module: 'app'}))
+    .pipe(gulp.dest('./build/templates'));
+});
+
+gulp.task('prepareDirectivesTempl', function () {
+  gulp.src(paths.htmlDirectives)
+  .pipe(gulp.dest('./build/templates/directives/'));
+});
+
+gulp.task('stubby', function(cb) {
+  var options = {
+    relativeFilesPath: true,
+    mute: false,
+    files: [
+      './src/mockapi/config.json'
+    ]
+  };
+  return plugins.stubbyServer(options, cb);
+});
+
+gulp.task('webServer',  ['stubby'], function() {
+  gulp.src('./')
+    .pipe(plugins.webserver({
+      directoryListing: true,
+      host: '0.0.0.0',
+      port: 5050,
+      open: 'http://127.0.0.1:5050/build/index.html',
+      proxies: [
+        {
+          'source': '/api',
+          'target': 'http://127.0.0.1:8882/api'
+        }
+      ]
+    }));
+});
+
+gulp.task('watch', function(){
+  gulp.watch(paths.jsFiles, ['prepareJS']);
+  gulp.watch(paths.scssFiles, ['prepareCSS']);
+  gulp.watch(paths.htmlFiles, ['prepareLibs', 'prepareTemplates']);
+  gulp.watch(paths.htmlDirectives, ['prepareDirectivesTempl']);
+});
+
+gulp.task('default', ['prepareJS', 'prepareCSS', 'prepareImages', 'prepareLibs', 'prepareTemplates', 'prepareDirectivesTempl','webServer', 'watch'], function() {
+});
